bd1e2c720e39b7374768ee108aaef754cdb7a0ac
Francisco Varela Cid <fvarelacid@gmail.com>
2022-03-18 19:17:32 +0000

    Update chat.py
    Added streamlit.
    Created bot within streamlit.
    Created flag for prev_tag.

--- Emergency_Chatbot/chat.py
+++ Emergency_Chatbot/chat.py
@@ -1,10 +1,13 @@
-import random
 import json
-
 import torch
-
 from model import NeuralNet
 from nltk_utils import bag_of_words, tokenize
+import nltk
+import streamlit as st
+from streamlit_chat import message
+import random
+
+nltk.download('punkt')
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -12,7 +15,7 @@ with open('intents.json', 'r') as json_data:
     intents = json.load(json_data)
 
 FILE = "trained.pth"
-data = torch.load(FILE)
+data = torch.load(FILE, map_location=torch.device('cpu'))
 
 input_size = data["input_size"]
 hidden_size = data["hidden_size"]
@@ -25,31 +28,83 @@ model = NeuralNet(input_size, hidden_size, output_size).to(device)
 model.load_state_dict(model_state)
 model.eval()
 
-bot_name = "Lennie"
-print("Let's have a chat! (type 'exit' to stop chatting!)")
+bot_name = "Gloria"
 
-while True:
-    sentence = input("You: ")
-    if sentence == "exit":
-        print("Thanks for joining me!")
-        break
+prev_tag = None
 
-    sentence = tokenize(sentence)
+##### Streamlit App #####
+
+st.set_page_config(
+    page_title="Emergency Chatbot",
+    page_icon=":robot:"
+)
+
+st.title("\U0001f691 Emergency Chatbot \U0001f691")
+
+
+
+st.write("You are talking to our bot nurse, {}".format(bot_name))
+
+if 'generated' not in st.session_state:
+    st.session_state['generated'] = []
+
+if 'past' not in st.session_state:
+    st.session_state['past'] = []
+
+if 'prev_tag' not in st.session_state:
+    st.session_state['prev_tag'] = None
+
+def get_text():
+    input_text = st.text_input("You: ", key="input")
+    return input_text
+
+user_input = get_text()
+
+prev_tag = st.session_state['prev_tag']
+
+if user_input:
+
+    st.session_state.past.append(user_input)
+
+    sentence = tokenize(user_input)
     X = bag_of_words(sentence, all_words)
     X = X.reshape(1, X.shape[0])
     X = torch.from_numpy(X).to(device)
 
-    output = model(X)
-    _, predicted = torch.max(output, dim=1)
+    output_model = model(X)
+    _, predicted = torch.max(output_model, dim=1)
 
     tag = tags[predicted.item()]
-
-    probs = torch.softmax(output, dim=1)
+    probs = torch.softmax(output_model, dim=1)
     prob = probs[0][predicted.item()]
-    print(prob)
+
+    print("Tag: ", tag)
+
     if prob.item() > 0.75:
         for intent in intents['intents']:
             if tag == intent['tag']:
-                print(f"{bot_name}: {random.choice(intent['responses'])}")
+                if prev_tag == "appointment" and tag == "affirmation":
+                    output = f"{random.choice(intent['responses'])}"
+                elif prev_tag == "affirmation" and tag == "affirmation":
+                    output = f"{random.choice(intent['responses2'])}"
+                elif prev_tag == "appointment" and tag == "negation":
+                    output = f"{random.choice(intent['responses'])}"
+                elif prev_tag == "affirmation" and tag == "negation":
+                    output = f"{random.choice(intent['responses2'])}"
+                else:
+                    output = f"{random.choice(intent['responses'])}"
+                st.session_state.generated.append(output)
+                print("Prev tag: ", st.session_state.prev_tag)
+                st.session_state.prev_tag = tag
+                print("Confirmed tag: ", tag)
     else:
-        print(f"{bot_name}: I do not understand. Try to be more specific")
+        output = "I do not understand. Try to be more specific."
+        st.session_state.generated.append(output)
+
+
+if st.session_state['generated']:
+
+    for i in range(len(st.session_state['generated'])-1, -1, -1):
+        message(st.session_state["generated"][i], key=str(i))
+        message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
+    
