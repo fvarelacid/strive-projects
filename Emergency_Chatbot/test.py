abde2e0614d2828c5ce97e18e3229be5677a49dc
Francisco Varela Cid <fvarelacid@gmail.com>
2022-03-22 12:30:19 +0000

    Update test.py

--- Emergency_Chatbot/test.py
+++ Emergency_Chatbot/test.py
@@ -80,7 +80,7 @@ while True:
     tag = tags[predicted.item()]
     probs =torch.softmax(output, dim =1)
     prob = probs[0][predicted.item()]
-    #print(prob)
+    # print(prob)
     if prob.item() > 0.75:
         for intent in intents['intents']:
             if tag == intent['tag']:
